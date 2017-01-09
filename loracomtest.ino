f8c7e7205a1830801d618e430ea4f3366aadc08b
A-n-d-y-W-e-n <a409052003@gmail.com>
2017-01-09 13:49:22 +0800

    refactor dl_msg function

--- loracomtest.ino
+++ loracomtest.ino
@@ -13,60 +13,18 @@ int readbuffersize;
 char temp_input;
 
 void setup(){
-
   Serial.begin(9600);
   Serial1.begin(9600);
 
-	dht.begin();
+  dht.begin();
   pinMode(LED_PORT, OUTPUT);
   pinMode(FAN_PORT, OUTPUT);
 
-	Serial.print(DHTTYPE);
-	Serial.println(" test!");
+  Serial.print(DHTTYPE);
+  Serial.println("test!");
 }
-void loop(){
 
-	if (dht.read())
-	{
-		tempC = dht.readTemperature();
-		Humi = dht.readHumidity();
-
-
-		Serial.println("------------------------------");
-		Serial.print("Temperature Celcius = ");
-		Serial.print(dht.readTemperature());
-		Serial.println("C");
-
-		Serial.print("Temperature Fahrenheit = ");
-		Serial.print(dht.readTemperature(false));
-		Serial.println("F");
-
-		Serial.print("Humidity = ");
-		Serial.print(dht.readHumidity());
-		Serial.println("%");
-
-		Serial.print("HeatIndex = ");
-		Serial.print(dht.readHeatIndex(tempC, Humi));
-		Serial.println("C");
-
-		Serial.print("DewPoint = ");
-		Serial.print(dht.readDewPoint(tempC, Humi));
-		Serial.println("C");
-
-		Serial.println("Ready to Send");
-		Serial.print("AT+DTX=11,\"T");
-		Serial.print(dht.readHeatIndex(tempC, Humi));
-		Serial.print(dht.readHumidity());
-		Serial.println("\"");
-
-		Serial1.print("AT+DTX=11,\"T");
-		Serial1.print(dht.readHeatIndex(tempC, Humi));
-		Serial1.print(dht.readHumidity());
-		Serial1.println("\"");
-
-		//Serial1.println("AT+DTX=11,\"12345ABCdef\"");
-	}
-
+void dl_msg(){
   if(Serial1.read()){
     Serial.print(Serial1.read());
     Serial1.print("AT+DRX?");
@@ -90,30 +48,51 @@ void loop(){
       Serial.println("FAN ON");
     }
   }
+}
 
-  // if(Serial1.read() == 'led0'){
-  //   digitalWrite(13, LOW);
-  //   Serial.println("Switch LED OFF");
-  // }
-  //
-  // else if(Serial1.read() == 'led1'){
-  //   digitalWrite(13, HIGH);
-  //   Serial.println("Switch LED ON");
-  // }
-  //
-  // else if(Serial1.read() == 'fan0'){
-  //   digitalWrite(FAN_PORT, HIGH);
-  //   Serial.println("FAN OFF");
-  // }
-  //
-  // else if(Serial1.read() == 'fan1'){
-  //   digitalWrite(FAN_PORT, HIGH);
-  //   Serial.println("FAN ON");
-  // }
+void loop(){
+  if (dht.read()){
+    tempC = dht.readTemperature();
+    Humi = dht.readHumidity();
 
-	delay(1000);
+    Serial.println("------------------------------");
+    Serial.print("Temperature Celcius = ");
+    Serial.print(dht.readTemperature());
+    Serial.println("C");
 
-	/*
+    Serial.print("Temperature Fahrenheit = ");
+    Serial.print(dht.readTemperature(false));
+    Serial.println("F");
+
+    Serial.print("Humidity = ");
+    Serial.print(dht.readHumidity());
+    Serial.println("%");
+
+    Serial.print("HeatIndex = ");
+    Serial.print(dht.readHeatIndex(tempC, Humi));
+    Serial.println("C");
+
+    Serial.print("DewPoint = ");
+    Serial.print(dht.readDewPoint(tempC, Humi));
+    Serial.println("C");
+
+    Serial.println("Ready to Send");
+    Serial.print("AT+DTX=11,\"T");
+    Serial.print(dht.readHeatIndex(tempC, Humi));
+    Serial.print(dht.readHumidity());
+    Serial.println("\"");
+
+    Serial1.print("AT+DTX=11,\"T");
+    Serial1.print(dht.readHeatIndex(tempC, Humi));
+    Serial1.print(dht.readHumidity());
+    Serial1.println("\"");
+
+    //Serial1.println("AT+DTX=11,\"12345ABCdef\"");
+  }
+
+  delay(1000);
+
+  /*
   Serial.println("Ready to Send");
   Serial1.println("AT+DTX=11,\"12345ABCdef\"");
   delay(1000);*/
@@ -123,18 +102,20 @@ void loop(){
     temp_input = Serial1.read();
     Serial.print(temp_input);
     readbuffersize--;
- }
+  }
 
- delay(9000);
+  delay(9000);
 
-   readbuffersize = Serial1.available();
+  readbuffersize = Serial1.available();
   while(readbuffersize){
     temp_input = Serial1.read();
     Serial.print(temp_input);
     readbuffersize--;
- }
+  }
 
- Serial.println("things");
+  Serial.println("things");
 
- delay(10000);
+  delay(10000);
+
+  dl_msg();
 }
